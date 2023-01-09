d1038ad7bbfd105277b5f244f488a22af44b90aa
Michael A <mikeade08l@GMAIL.COM>
2023-01-09 10:33:13 +0000

    adding posts

--- controller/user.go
+++ controller/user.go
@@ -1,10 +1,27 @@
 package controller
 
 import (
+	"forum/dbmanagement"
 	"html/template"
 	"net/http"
+	"time"
 )
 
+type Data struct {
+	ListOfData []string
+}
+
 func UserLoggedIn(w http.ResponseWriter, r *http.Request, tmpl *template.Template) {
-	tmpl.ExecuteTemplate(w, "user.html", nil)
+	if r.Method == "POST" {
+		comment := r.FormValue("post")
+		if comment != "" {
+			dbmanagement.InsertPost(comment, 1, time.Now().String(), "general")
+		}
+	}
+	posts := dbmanagement.DisplayAllPosts()
+	data := Data{}
+	for _, v := range posts {
+		data.ListOfData = append(data.ListOfData, v.Comment)
+	}
+	tmpl.ExecuteTemplate(w, "user.html", data)
 }
