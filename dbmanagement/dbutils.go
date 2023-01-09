9cc198351ec1288763b8a8958166d016b780cd9b
Michael A <mikeade08l@GMAIL.COM>
2023-01-09 10:57:58 +0000

    editing in progress

--- dbmanagement/dbutils.go
+++ dbmanagement/dbutils.go
@@ -142,6 +142,95 @@ func CreateTable(db *sql.DB, table string) {
 	statement.Exec()
 }
 
+func InsertUser(UUID string, name string, email string, password string, permission string) {
+	db, _ := sql.Open("sqlite3", "./forum.db")
+	defer db.Close()
+	log.Println("Inserting user record...")
+	insertUserData := "INSERT INTO Users(UUID, name, email, password, permission) VALUES (?, ?, ?, ?, ?)"
+	statement, err := db.Prepare(insertUserData)
+	if err != nil {
+		log.Fatalln("User Prepare failed: ", err.Error())
+	}
+	_, err = statement.Exec(UUID, name, email, password, permission)
+	if err != nil {
+		log.Fatalln("Statement Exec failed: ", err.Error())
+	}
+}
+
+func InsertPost(text string, user int, time string, tags string) {
+	db, _ := sql.Open("sqlite3", "./forum.db")
+	defer db.Close()
+	log.Println("Inserting post...")
+	insertPostData := "INSERT INTO Posts(comment, user, time, tags) VALUES (?, ?, ?, ?)"
+	statement, err := db.Prepare(insertPostData)
+	if err != nil {
+		log.Fatalln("Post Prepare failed: ", err.Error())
+	}
+	_, err = statement.Exec(text, user, time, tags)
+	if err != nil {
+		log.Fatalln("Statement Exec failed: ", err.Error())
+	}
+}
+
+func DisplayAllUsers() {
+	db, _ := sql.Open("sqlite3", "./forum.db")
+	defer db.Close()
+	row, err := db.Query("SELECT * FROM Users ORDER BY name")
+	if err != nil {
+		log.Fatalln("User query failed: ", err.Error())
+	}
+	defer row.Close()
+	for row.Next() {
+		var user_id int
+		var UUID string
+		var name string
+		var email string
+		var password string
+		var permission string
+		row.Scan(&user_id, &UUID, &name, &email, &password, &permission)
+		log.Println("User: ", user_id, " ", UUID, " ", name, " ", email, " ", password, " ", permission)
+	}
+}
+
+func DisplayAllPosts() []Post {
+	var posts []Post
+	db, _ := sql.Open("sqlite3", "./forum.db")
+	defer db.Close()
+	row, err := db.Query("SELECT * FROM Posts ORDER BY time")
+	if err != nil {
+		log.Fatalln("Post query failed: ", err.Error())
+	}
+	defer row.Close()
+	for row.Next() {
+		var post_id int
+		var comment string
+		var user int
+		var likes int
+		var dislikes int
+		var time interface{}
+		var tags string
+		row.Scan(&post_id, &comment, &user, &likes, &dislikes, &time, &tags)
+		log.Println("User: ", post_id, " ", comment, " ", user, " ", likes, " ", dislikes, " ", time)
+		posts = append(posts, Post{comment, user, likes, dislikes, time})
+	}
+	return posts
+}
+
+func SelectUniqueUser(userName string) User {
+	var user User
+	db, _ := sql.Open("sqlite3", "./forum.db")
+	defer db.Close()
+	stm, err := db.Prepare("SELECT * FROM Users WHERE name = ?")
+	if err != nil {
+		log.Fatalln("Statement failed: ", err.Error())
+	}
+	err = stm.QueryRow(userName).Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.Password, &user.Permission)
+	if err != nil {
+		log.Fatalln("Query Row failed: ", err.Error())
+	}
+	return user
+}
+
 // uuid version that's allowed from packages outputs something of type UUID
 // which needs to be converted to string to be used as we have been using the UUIDs from the other package
 func GenerateUUIDString() string {
@@ -175,17 +264,16 @@ func InsertUser(name string, email string, password string, permission string) U
 func InsertPost(content string, ownerId string, likes int, dislikes int, time time.Time) Post {
 	db, _ := sql.Open("sqlite3", "./forum.db")
 	defer db.Close()
-	log.Println("Inserting post record...")
-
-	UUID := GenerateUUIDString()
-	insertPostData := "INSERT INTO Posts(UUID, content, ownerId, likes, dislikes, time) VALUES (?, ?, ?, ?, ?, ?)"
+	log.Println("Inserting post...")
+	insertPostData := "INSERT INTO Posts(comment, user, time, tags) VALUES (?, ?, ?, ?)"
 	statement, err := db.Prepare(insertPostData)
-	utils.HandleError("User Prepare failed: ", err)
-
-	_, err = statement.Exec(UUID, content, ownerId, likes, dislikes, time)
-	utils.HandleError("Statement Exec failed: ", err)
-
-	return Post{UUID, content, ownerId, likes, dislikes, time}
+	if err != nil {
+		log.Fatalln("Post Prepare failed: ", err.Error())
+	}
+	_, err = statement.Exec(text, user, time, tags)
+	if err != nil {
+		log.Fatalln("Statement Exec failed: ", err.Error())
+	}
 }
 
 func InsertComment(content string, postId string, ownerId string, likes int, dislikes int, time time.Time) Comment {
