3442d189c4f80f759cc4a2d1f49f798eda5bcbe8
peafield <peterjohncoles@gmail.com>
2023-01-09 09:49:12 +0000

    remove unnecessary files

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "forum/dbmanagement"
-
-func initaliseDatabase() {
-	dbmanagement.CreateDatabase()
-	// dbmanagement.InsertUser("0001", "8423479283", "Jupiter", "Koberich-Coles", "387493874")
-	// dbmanagement.SelectUser()
-}
