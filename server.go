17ea675e264c2d90ce237e090f16dd040eb690da
Michael A <mikeade08l@GMAIL.COM>
2023-02-02 15:46:26 +0000

    created route protection file

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/tls"
-	"forum/controller"
 	"forum/dbmanagement"
 	"html/template"
 	"log"
@@ -15,37 +14,6 @@ var tmpl *template.Template
 func init() {
 	tmpl = template.Must(template.ParseGlob("static/*.html"))
 
-}
-
-func protectGetRequests(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			controller.PageErrors(w, r, tmpl, 400, "Bad Request")
-			return
-		}
-		h(w, r)
-
-	}
-}
-
-func protectPostRequests(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			controller.PageErrors(w, r, tmpl, 400, "Bad Request")
-			return
-		}
-		h(w, r)
-	}
-}
-
-func protectPostGetRequests(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" && r.Method != "GET" {
-			controller.PageErrors(w, r, tmpl, 400, "Bad requests")
-			return
-		}
-		h(w, r)
-	}
 }
 
 func main() {
